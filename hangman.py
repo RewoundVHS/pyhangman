c9c8defa9f27502ce59e07c9f388e9e3c3d1bbdd
RewoundVHS <nevelaughery@gmail.com>
2018-05-30 22:39:25 -0400

    Initial commit

---
+++ hangman.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+
+import hangmanart
+from wordlist import words
+import random
+
+findWord = words[random.randrange(len(words))]
+hidden = list('_' * len(findWord))
+
+wrongCount = 0
+maxWrong = 7
+
+print(hangmanart.art[wrongCount])
+
+while((findWord != ''.join(hidden)) and (wrongCount < maxWrong)):
+    enteredChar = input('Enter a character: ').lower()
+
+    if enteredChar not in findWord:
+        wrongCount += 1
+        print('The letter', enteredChar, 'is not in the word')
+
+    for i, c in enumerate(findWord):
+        if c == enteredChar:
+            print(enteredChar, 'found at', i)
+            hidden[i] = enteredChar
+
+    print(hangmanart.art[wrongCount])
+    print(''.join(hidden))
+
+print('The word was', findWord)
+if findWord == ''.join(hidden):
+    print('You win!')
+else:
+    print('Better luck next time!')
---
+++ wordlist.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+
+words = [
+'zenith',
+'programming',
+'archway',
+'modular',
+'excavate',
+'rhetorical',
+'tenacious',
+'paradigm',
+'nostalgic',
+'misanthrope',
+'lucid',
+'jurisdiction',
+'hyperbole',
+'cacophony',
+'cardinal',
+'cosmopolitan',
+'perpetuity',
+'galvanize',
+'quixotic'
+]
