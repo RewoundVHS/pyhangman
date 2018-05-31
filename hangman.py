b3db6e9e7697d09dee0ccbe92a3581f234196b62
RewoundVHS <nevelaughery@gmail.com>
2018-05-31 19:53:51 -0400

    Added ! to solve and only accepting alphas

--- hangman.py
+++ hangman.py
@@ -10,25 +10,36 @@ hidden = list('_' * len(findWord))
 wrongCount = 0
 maxWrong = 7
 
+guessed = False
+
 print(hangmanart.art[wrongCount])
 print(''.join(hidden))
 
-while((findWord != ''.join(hidden)) and (wrongCount < maxWrong)):
-    enteredChar = input('Enter a character: ').lower()
+while((findWord != ''.join(hidden)) and (wrongCount < maxWrong) and not guessed):
+    enteredChar = input('Enter a letter or ! to solve: ').lower()
 
-    if enteredChar not in findWord:
-        wrongCount += 1
-        print('The letter', enteredChar, 'is not in the word')
+    if enteredChar.isalpha() and len(enteredChar) == 1:
+        if enteredChar not in findWord:
+            wrongCount += 1
+            print('The letter', enteredChar, 'is not in the word')
 
-    for i, c in enumerate(findWord):
-        if c == enteredChar:
-            hidden[i] = enteredChar
+        for i, c in enumerate(findWord):
+            if c == enteredChar:
+                hidden[i] = enteredChar
 
-    print(hangmanart.art[wrongCount])
-    print(''.join(hidden))
+        print(hangmanart.art[wrongCount])
+        print(''.join(hidden))
 
-print('The word was', findWord)
+    elif enteredChar == '!':
+        guess = input('Enter your guess: ').lower()
+        hidden = list(guess)
+        guessed = True
+
+    else:
+        print('Please enter a letter')
+
 if findWord == ''.join(hidden):
     print('You win!')
 else:
+    print('The word was', findWord)
     print('Better luck next time!')
